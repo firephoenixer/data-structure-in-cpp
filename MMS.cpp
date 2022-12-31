ad4d918f1f99e58d9eab60499bf14a360b54e9a6
firephoenixer <owen00721@gmail.com>
2022-12-31 10:30:14 +0800

    create MMS

---
+++ MMS.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string.h>
+/*  Member manage system
+Let's image there is a organization need a MMS.
+We use this MMS to test Datastructure and some algorithm.
+*/
+
+int main()
+{
+	std::cout << "Member manage system is running!" << std::endl;
+
+
+
+
+
+
+
+
+
+
+
+
+    std::cout << "Member manage system is finished!" << std::endl;
+	return 0;
+}
